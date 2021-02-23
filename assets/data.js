265a6529fb6fbad6682c41c7c057636493561620
abrokwas <tkromeo2003@gmail.com>
2021-02-23 15:55:34 +0000

    function, frontfetch, and data created

--- assets/data.js
+++ assets/data.js
@@ -1,39 +1,57 @@
 const items = [
   {
     id: 'recmg2a1ctaEJNZhu',
-    name: 'utopia sofa',
+    name: 'mini drone',
     image: {
       url:
-        'https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg',
+        'https://lh3.googleusercontent.com/8pZqHSAetGQrvGs5IVGBOQcpXUn5cQXnae8Ta41c6-k_jwCsPpDPXXwzJ1sQsb_KCKqHMR--GdDIo_FVZuGtL29y2iN5RUD0N1nEM6zLfNBiLUy6Oc4vifUYWYrOxhXrF0gNUp4xL1oDd6xPpPkh1_h8O0XwKa82wBnue9ikfNfilg6XTU7LT1qPIzxp-Ordh2HUKg0B2oSPGZFst6WP22ROSpr78h0moPMxx4d65A4KwetIvgQ8qq9GEAHo9aMBJVZjbxyAotUA7lVm_J5xz79TdZPCPTFCk81Zpm5tsKkSrE1_DwgvbHpnsNGvJRricr5HBMN7n01HxtzACxeG6op1P6_HNsm-hAL6B0-cfx6j6LdTCZ2Q1KxvKZ3xBbmnnQbQfikbNl1ICpzXT6X1x6qsmgHocE-iI4WKaDn8yPaU6UphwKH0fFsDEqBD8L6aH6O18MuI10Y53nGQsH63YMRMFmIU5RwJr8LrBxNEeycQpJTBSKS1b6GUHYbpWZ7-KkcyqHaIn5Vu_o6icM_UDEJ1AJoMAbfOMU2sMUh_zlnJ8UXsbtk57-V0zjXX-Frer2IOzS8yD2NZm8l9f1dgWHPf2lmCx9HmO_vWFGDZl3TzvwRSxUl11cK_3qlmuMgSuQweKRwsJo2sVpwiwT5KnZgPSg4nxqgB4XW2bHG1he0nhFdBhZ-M79KHTyE=w1410-h1368-no?authuser=0',
     },
     price: 39.95,
   },
   {
     id: 'recvKMNR3YFw0bEt3',
-    name: 'entertainment center',
+    name: 'babyliss hair clippers',
     image: {
       url:
-        'https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg',
+        'https://lh3.googleusercontent.com/L4_5y1_Ogza8rDwWa_5MhURXZ_1CihGbmPVB2vEm_Ru5NiPOnny_N8lV8iNEMrS2h31iD6yU0AgVfQQGcXN4fbFMhRtVviEi6bXOzRSl5m2qE2jGLTcperZT8pqPG4PQIdRoCr5aMIn2Eqdml6po5giRaRq-y0ljgNgh06UItNgpbKn7DSa_bKeRR9S9E-uMDoMC5SiDl2pYzEF8R13lke4qA3vy3KuRoHgxcNQNnq2-0Dqj5WZQGK6Nvsk_SC-5BpvORxHAbFcFQrruKQhpDasfdf3aiuJ8133MdGjzKoo-DWaGYIgNfqRSVeumzIRkAPOfmqK-mjKNvF815KblVL9s35lSRyOPFTnM3pkYuKH_C_qcluVj479qQeCkZG3wiJyR-9DCIJ_56rfFdxtZODtUkxMGXth0h4R_sHygleW84rVl4yhUw4LINVRg5nKuFWYNTRswETMdzRKzs2QZmVlgv8rkUsO-ikeWir0ISbmNn8y7mN3pgTjAHjyfDwWzflR2dHJ-jioVst2eCqRscXGaTXXsrs5IlyOoZajNfO3pbqG8hJAp2aBEiOnmVAWHawYq_YGCwXYBNgnKM-CKCP-31TB7YvR7sfdbACh6fm2CJ-g6T3vzD7zMYD9i50GdNB-34vqr8JhLaKt2yivMsmR1dxTTDBncpp3mn72ho83rQZJbL_jVTIapdhI=w1324-h1368-no?authuser=0',
     },
     price: 29.98,
   },
   {
     id: 'recxaXFy5IW539sgM',
-    name: 'albany sectional',
+    name: 'duracell AA bateries',
     image: {
       url:
-        'https://dl.airtable.com/.attachments/05ecddf7ac8d581ecc3f7922415e7907/a4242abc/product-1.jpeg',
+        'https://lh3.googleusercontent.com/3hVKecM3kl1fl6zp1lpFbUCKcWQsPhVIC0nZUqIlyRhLd1TZXWzBKA9Qvi8UpOSMQMTEcGtUtvmVrV3RrhrC0D1KOCAGptoRViCN2dBTojlb3nG8hBj6PO-BSNCtY_nhPOXLURjEtAZkjNED0QST5G9PpmSXKa99atOpu-pBwRBiSwImNEG23MSHefWyXDssYIVT3FqoRaZqIsM_7ibZv3-zQk8M2nvx9d1jnEr8cw2Oc1_Hk2oRABmqvovGok8VEIByXwylw-dwnRmb3jBnV_TTzTv1pyXT-bX01eWSBtWnCTU8dwaB0LLwudp3gVNtOQHjnyLV0srNUSC-bDThjCr_ZJynd2KhR5QhRJYS0S6luaAQhOlF8_oXkRE5q0SFFuIoPsf56-baN4A4tq-P89GyQVy-u3MBfhP0NbA8st9f8nJ0E-fEmiVupv67hRGNBWRShKToqFzZdMgWhd9Aqde9qtXkV09qO-toQkeOZvwkvqfwVqBswj37eA9m-x-9XPmPhP9IUtSrxMNZ2zxiacZO3YElZtBKjn7klGTWARI5K97gICVW5R_hOWsRdlvpzQ_Y15Bs3yV6WulT0qodo_-P4dCLPGpRG_CdjvPwOOuu0nJKnAeT_MFOrrEnBB5LjD3IG7EW46h8egiK9OW644u5mexD4aQ9A51FsqJEM34Wa_Qvc2WiFjiglDA=w1500-h1057-no?authuser=0',
     },
     price: 10.99,
   },
   {
     id: 'recyqtRglGNGtO4Q5',
-    name: 'leather sofa',
+    name: 'drone with cam',
     image: {
       url:
-        'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+        'https://lh3.googleusercontent.com/G3vtzByJ0VAl6jFGDtp2BDGIZGXbBZVtu6ZiDBdAzbrM_GPeIPO7kvz_3EXfTkO6Tf94XH13UaixzP4og5O_xR3AKwnj3GaLk1tolVZ9gp6NAGTuOxXAjDlPhfGEIGONaxpswdOVRMh8CUDtA3P_gsUtKqrgZkJKB2s8MXorZkHFgz1m22zLPzySeGuaOb-dvZYSEa-jBDhx7P193g-AokZXD40ORPZWdAGvRo0Yf4avNuE_BiQq8RrUoKfXjjrK_K1WQnVyVNt_R2MgATqTcZu8Nv5wlLwE-z-y77cpeCh2djLKK7RByzwEffA9cI7NjxY5ohl1JiBsP0kAcViZLEUfRL5VRFSa1mDKRR3eOV6GZ6UDWFhIQ4D40aUQlJIAV5-aNVN4LAVWDduvyY9oSw0ySPESxgqZ9iK0LpWGxE5imcMxDrAnQLS3lP3XeDzS5HjLxGy_lr1R0d8nLkkagmOxBj5L7ntZ5iYDF-ne5HpNFATypZJz3FPJ9EDGq1lHgNVGl-YmwCPtIQAU_OEiAYRUmEw4V8qusOnRV-vnoVp3gowd-ykYib2qIoBzuMYMQaige1mC2SoBBmnoaN8c4CSXRhcrKq2yUDkQIlSYLEg9q8gpsCYaI6AVCuBRhgH72NfTbdWQXhA5QEOjDYgd5hOCjdPj8FpbhMUfdg3UJOl90k_ZoWSHWGsy3-0=w1312-h1368-no?authuser=0',
     },
     price: 9.99,
+  },
+   {
+    id: 'recyqtRglGNGsO4Q5',
+    name: 'Golden Butter',
+    image: {
+      url:
+        'https://i.pinimg.com/474x/c6/87/af/c687afce80fb039c1a7552ed7b07251e.jpg',
+    },
+    price: 4.99,
+  },
+   {
+    id: 'recyqtfglGNGsO4Q5',
+    name: 'Phone Magnifier',
+    image: {
+      url:
+        'https://lh3.googleusercontent.com/ektoz920ooeqrEN8xFZYtbMrrHamcj-wCByu4V47iogflycW5PrbBrd4UcdeoTdPjxFIkIrJZci_EnYEokH7VVaPbJSl-Ix-8MLSYEA9IpgYpPQ3lamxt3ZIC77KkUfq0EsK-6BVAxtPy5gvmUPCEXZEQAjVf33ElyqGxmNaDGbTiq4grRrpmYJ-gtGSNjY6ojEX_GanMA8PxfKMAFuB9jXFdnGDK40bdUYmCkFSLTFYxLE1G7aS4PIRa51xq4SibpO0NKmbHUBG1IDPV4vfnPhiuVdNoZdGhMEQ0xGjVb9Jf2zuoLO4ZsqKRXCjBRkib-IKfvbmbEJlDtICq-nJk4pYgcDyy_Lki6QnvZmSk66b82oF1wgo1aCOYIZlJuLKbqroDfDKKuAnsNOyWwJb_XAGOPIN9PmEf9dBTyY0cHLnDBF3b4MNtvUCTn-rvZn1P32d6OJZ0XjfPWRz60uBkbjfFPD4o1i62BVr8bNhTPjw6lXbCuuFhrfzEkmhbVPi9kNI2Eq62S_YVB73H4B4F23u9_iBqAmBWhj51-1dr7KclhoTj2LFT7HIy5A3vTB_Yh07d3m2lPp_ToxBZJxPllraPVsZ0vadDdX4uzK1dvt7K65XGTmvAdZzm1C6NadPn-8nZvhGzXXjH_MBbnxVhQ10ac8I9mc3dyYLZy8AMaZHIR2HcxhcdhZ1vMk=w692-h732-no?authuser=0',
+    },
+    price: 16.99,
   },
 ]
 
