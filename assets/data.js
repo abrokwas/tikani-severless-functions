a2222a413831a427042f09c35254f502d50a312a
abrokwas <tkromeo2003@gmail.com>
2021-03-10 21:45:04 +0000

    data and styles updated

--- assets/data.js
+++ assets/data.js
@@ -13,7 +13,7 @@ const items = [
     name: 'babyliss hair clippers',
     image: {
       url:
-        'https://lh3.googleusercontent.com/L4_5y1_Ogza8rDwWa_5MhURXZ_1CihGbmPVB2vEm_Ru5NiPOnny_N8lV8iNEMrS2h31iD6yU0AgVfQQGcXN4fbFMhRtVviEi6bXOzRSl5m2qE2jGLTcperZT8pqPG4PQIdRoCr5aMIn2Eqdml6po5giRaRq-y0ljgNgh06UItNgpbKn7DSa_bKeRR9S9E-uMDoMC5SiDl2pYzEF8R13lke4qA3vy3KuRoHgxcNQNnq2-0Dqj5WZQGK6Nvsk_SC-5BpvORxHAbFcFQrruKQhpDasfdf3aiuJ8133MdGjzKoo-DWaGYIgNfqRSVeumzIRkAPOfmqK-mjKNvF815KblVL9s35lSRyOPFTnM3pkYuKH_C_qcluVj479qQeCkZG3wiJyR-9DCIJ_56rfFdxtZODtUkxMGXth0h4R_sHygleW84rVl4yhUw4LINVRg5nKuFWYNTRswETMdzRKzs2QZmVlgv8rkUsO-ikeWir0ISbmNn8y7mN3pgTjAHjyfDwWzflR2dHJ-jioVst2eCqRscXGaTXXsrs5IlyOoZajNfO3pbqG8hJAp2aBEiOnmVAWHawYq_YGCwXYBNgnKM-CKCP-31TB7YvR7sfdbACh6fm2CJ-g6T3vzD7zMYD9i50GdNB-34vqr8JhLaKt2yivMsmR1dxTTDBncpp3mn72ho83rQZJbL_jVTIapdhI=w1324-h1368-no?authuser=0',
+        'https://lh3.googleusercontent.com/9fMr2IuTym2P0ad0sKska_U6TD2tyOwQGgkZ9Et_naQJmkcMXFLd0o7lWZu327tU-UMM1miFmcLtMmknlWsB8cd2iJPV6EVOTs8BCiTX_RUufhasKjtw2k53osFS-x4cVCrmgwsLhZoRls5xscNRC83_CriLxSDUKj4DLefwkoufiguMGzbs1MqRpSJZY-rpzP_C4YVbZfZZ1QrTVuwNwTtBuz668qAI2TLEKMX3dpFqIuDZkezbWuAeWkDJxxU-i95_1ls6ukOZRtDvwvz2pdMagy6dFGWhV2JCFvlTjL2TGz6n_CNEn-nbTq9Xj7xucLPHNy1tv7DsOuLWScfoj1I__oG-PZsicd5Eyc5kS8_5UrXXQyuzzvgvY9r38Q_NLbl8HJot2IxBY3MLUpbN5A0KhntJ5x_5Q4Ij031IqEY7Y1fyrqwDGJBH2dvK99OnEDv9hy2yDIRu2hOQjXdaIABiFWMUbcTAwRK2dhovJHf39cLw4u7F9XT87YwTb8dXld5IQHfifYIf1jonI8O_nNj0w5eKPEjae5iOZBbi8sXJ5HyJeF_gy_ETrXIJfYCrSUmBNQ6HGTP2y1FV-OFZ_WN_ZiGNCEIhndGYuW61cnnkr-b7zSu1mUgv80DHI6BzmMsOCyjBW4UBvJ4Z5ip8StWlw3RVGpMNNZOWphAADLqkcJLyC28BGx9ZrCg=w1550-h1258-no?authuser=0',
     },
     price: 29.98,
   },
@@ -52,6 +52,69 @@ const items = [
         'https://lh3.googleusercontent.com/ektoz920ooeqrEN8xFZYtbMrrHamcj-wCByu4V47iogflycW5PrbBrd4UcdeoTdPjxFIkIrJZci_EnYEokH7VVaPbJSl-Ix-8MLSYEA9IpgYpPQ3lamxt3ZIC77KkUfq0EsK-6BVAxtPy5gvmUPCEXZEQAjVf33ElyqGxmNaDGbTiq4grRrpmYJ-gtGSNjY6ojEX_GanMA8PxfKMAFuB9jXFdnGDK40bdUYmCkFSLTFYxLE1G7aS4PIRa51xq4SibpO0NKmbHUBG1IDPV4vfnPhiuVdNoZdGhMEQ0xGjVb9Jf2zuoLO4ZsqKRXCjBRkib-IKfvbmbEJlDtICq-nJk4pYgcDyy_Lki6QnvZmSk66b82oF1wgo1aCOYIZlJuLKbqroDfDKKuAnsNOyWwJb_XAGOPIN9PmEf9dBTyY0cHLnDBF3b4MNtvUCTn-rvZn1P32d6OJZ0XjfPWRz60uBkbjfFPD4o1i62BVr8bNhTPjw6lXbCuuFhrfzEkmhbVPi9kNI2Eq62S_YVB73H4B4F23u9_iBqAmBWhj51-1dr7KclhoTj2LFT7HIy5A3vTB_Yh07d3m2lPp_ToxBZJxPllraPVsZ0vadDdX4uzK1dvt7K65XGTmvAdZzm1C6NadPn-8nZvhGzXXjH_MBbnxVhQ10ac8I9mc3dyYLZy8AMaZHIR2HcxhcdhZ1vMk=w692-h732-no?authuser=0',
     },
     price: 16.99,
+  },
+  {
+    id: 'recydtfglGNGsO4Q5',
+    name: 'ginger nuts biscuits',
+    image: {
+      url:
+        'https://ui.assets-asda.com/dm/asdagroceries/5054781745563_T1?defaultImage=asdagroceries/noImage&resMode=sharp2&id=ef-Sx2&fmt=jpg&fit=constrain,1&wid=256&hei=256',
+    },
+    price: 0.99,
+  },
+  {
+    id: 'recyktfglGNGsO4Q5',
+    name: 'chocolate digestive biscuits',
+    image: {
+      url:
+        'https://lh3.googleusercontent.com/-Vp0PGcznLJq5T7N6SJ0PY8W7Vp-lAaBZ-PGFXsfEJotgYG7GPhCZ5325covv1NC3_s6mjGOiPjvsiCgW-2-L88AASY868zIEYVDwwvUeC4UfTwuxCpVjdd1D9uyOL_DciMjz0COgSrQQmlt94atDywLhllhFPf8UyOwCuRL3YCqORykhexeT1hVMTNVSQHJWSteLLvFiL_E7YdGJg6DWdbycO2x2N4D7ptN-R4WuLdExmHIDsvR0bvkE_5BYOM71fxDcpWin-TUlVjk7QAKuDnYMRPmKz7MdTA3xnFMPO5zFPKkeNe7XvQuBLjhK7rPWYw69E1JbdwoYYOX657kbKqqra7MYht2I6phNiOexuGASJgdX7_VdkNM2zmcwt3CPCsiMANNPaDMNe9VdR0pAAECOniCDYabJroti_o0gU34-11u5lBOG7OA4hsehmzM5WecZJqiQMLDyjYQxNeM-0w-R9EbwoH-e9WmWgGQowGMrBV1EpbrZlfk5w0gYtyS3gZ6j7QgG_yaRdVczPhGLUA6k3mqiqIjgPyKV1NBkXmdqLpnDgbLDnVnR0_jGO-lyNpSJK2m8jFsZ4XYJb1emc45y4V9pe4vnYpPSz1oUhx5hcT0iZiHkJ0vs36GXu-nRduBTi_tXGLpN8-o1-Q8l581f_0NNyx-YEDe0eSad9Glx-ux8mW8Azj-DT1tBx2_zlafkU9DXYpTRpgEqB_Wpw=s256-no?authuser=0',
+    },
+    price: 0.99,
+  },
+  {
+    id: 'recydtfglGNGkO4Q5',
+    name: 'bournbon biscuits',
+    image: {
+      url:
+        'https://lh3.googleusercontent.com/__HsimNjUVWY7bvE17sO8LfuDDvIYK5GekugjXjpV3RMmXgSVyVJFHqSxmPxCReg8ZcJJh_nAnexB1vl5dBgtRgKADvItWSJgYEjR6UmLWNLAfM8hCuM-qqvryGYlwXfeIy2Q_LuzYGqNMq6dK-1g4HaUd25-JD984fugtaeprgoyf7XPre9TlASK-i8L2qKqkEZ9St5fE50jQw4M_0Co4Zrbhf2RyJvCSdJqTVIjvL45-WMptytph4hljKtfcK2DEWuKe_eLM1EKCD45wyG17lQxVeNHeZhp_qAgRpD-o28ZlM00lCmuA5jbRIUs3VHgAunZPjMqcfuWkfdD721TXfd0ico5oAbZv7XfgHkuiLtwMknYevy_8QhE5M3q1l4olT4TqzB-cnoOIH1bjDWVEenH1OxtnC5OqgHJ24_asmoZAyhyqW52RrLehXfQTM4AywvgFHKGkEEjuQkrsjLR9F9433WmOi-4azpmPOcp2F_ldUH-LPXTfOPqypQ3Fvw2FF9EAqB-Px-YMdaflV56lJi8kSmY_5oyadAnfE2f1EDeAQVEnsAZ5wyLi3S9xtCk_mGgFiEy9hECmud_5SGD1BGua--2uc5m0KSdpaDVvMaDKicgiWbTQ2dc7g_HR9lMt4UwSy-jdbjKqdHTZjKjR_A58440N69BS_ZJ3LnvqNdWkEmad7E2vp9nvkOHuTjcb_3Onn-NHFO608orvYE0Q=s280-no?authuser=0',
+    },
+    price: 0.99,
+  },
+  {
+    id: 'recydtfglGNGlO4Q5',
+    name: 'digestive biscuits',
+    image: {
+      url:
+        'https://lh3.googleusercontent.com/F_E-sEusosD3rTzZBoxjft5edZdefiFJfi6Ev-NP6oJOkt4JaOKLNeO7PdA_LxKgrBT2yr4z6mfZpc8VGliCw--J8-6caFhLVkLxaCIqu5uqSiYO32pQID1bvz8BSVUzieDC_Lx4CEi3n4ypJBEvb5-5U6-vTXIH-iE5xkwhVC_lXPujDX81PlIbYYMxA92Ck4qQ8E0TNsIeeOnkIZE9l9_vdj0XtN1XRr5oFb0PbguiECJGPad86tz3M3VmWUrLI8-DiG0-BSrvIQqSV7RBGuQguHCpSBI7hwtVve_EBu59sM3IQQ5AQzZJxqA_rK3a7IS34xAhfHbomua0XwLXinqZa07cMHHisuDNM8N1weXwr_vKMUZD7x23zfnqJFaon_i4gy6sPSi_7P8k5ezMO_IkxF_JUjvC_jv_sO9aBFA6vV1XBXsQQOWwzt2u0Fly4BJ8gZrj5ozf2v22D-dh0cJ4TuRzm-0qEUPD_DRfrQWWY3I1rIdkBSq1I6Hve9f2pHuDO1m8060qSHDPdRAOGBil9RMKSZed5LeYH4nkHAJyKHdEwDEUV1E7iAMFxaR1OHDoWioTM32uTc3yISGEzb1He73cBt152pvJYFQS6jguKtFNjx58fMSPDe38EvQMs_KsfdCmPDz3rumvuakzWX0sW5L0gCgJMRSMmN7_ZfT9bExguxKzgX9mJqB5rdmaRELO30ctMPn93fosVcLTBg=s256-no?authuser=0',
+    },
+    price: 0.99,
+  },
+  {
+    id: 'recydtfglGNGsO4Q5',
+    name: 'oats crumble biscuits',
+    image: {
+      url:
+        'https://lh3.googleusercontent.com/wRHcFZx70isnBdC-omShSydQfSSwC6OCfYAWzRy6-JY_AOWimVvCFn_gyFscHh1dCukgQBfXBEdsiG7067EeR5uTUCp0EQKrOQVzmsUe0vY8-HKEk5_QrYX5cSCf5MMqe3P1VGKLFUlyqem3K5V1eU2k_ww8E7RokdC5vTuAeKiBAYFW53OSHPDfVdQzsTrqvC1kh-r_WuIREmcpxAfYPZ43_6bwaglG-FsARQfx2n5YceQA4UWwPf-LRloQPwUpmDCpR8OnVGQFdxkDX9zWISlgaOMLQunmiXGy7b1yr9lX7wTQMwsuhi9GLyb5xls9x2w2tSYJZl80NgptUSphK9tkIpdg1_BcJ3jvnw7JNhu4MnewRpqnSaKHEzmI84cT1V_hh-S_K5IvuhUF2vcC3OWikj386LNMpO4M2Eo8xQy_zmkiacGr1KuooZSAsp6GBHwUYsdFMQ_tX7tm5mZqIDPA3_-MLywLMH1kGw48ZYQuHSm1ajXk4yAVK00nHSCf4zDBu2l1u-pkTNUmWLgkNWeX9PgFue-pCcFNdhAEO0q9xD2RnaK1udRi_YIBUvHvAH4tPx4k30mJKlG_k9r-ah-b3pGnxAwWTaLsd5t_udZdsqLJSiv4BO4Nh2ptmQYyVen8oBs9-S97P5BkorD-g_x_r7B0HPryW2iXGrf-Ii6AedfKX07LzxoF4ay4ae2CK5NP2SCi3ryaYaSe1ULp1g=s256-no?authuser=0',
+    },
+    price: 0.99,
+  },
+  {
+    id: 'recyktfglGNGsO4Q5',
+    name: 'malted milk biscuits',
+    image: {
+      url:
+        'https://lh3.googleusercontent.com/mKTNLghCq1WHtW9QHNQQM2-EisnBn4HpNZHgbAotbDhqaUurPFhG1r7Le4LmomEmeDPV5NZXgnQdJrbhulZfAi-LieKyrOuAAej25q_iqBfUNLTCvhPQJNS1jSjPHgJjJ0XMQjQd1veAwFE2SdkEZgSNQHohRJBlneGPHv0GAl6VTmw9FwIo82wWTg67C98FHasMa01g_wI-WHKbcYsm_rK3jdLJT7LON3a4UG2qjMUFgtXgh-q68o__vzydQe1GwcPoCfew7xFoX6iG5OvfKWW3TJHAgM3EGdfxsCu3SSp-d3UftZuKqgY7QxCoZtcObJsI_pmxKJOGuE57BTkSWXAf-l3ryLT8u2594TfEflhbda3sadvAIJI3YvZMbcuHS0n6i1NDMF56Pcpv-CjNVWgr-iUyvC7xMOcFXq3qyi8gNk-xUtHbprhXXsQ6KlLt0ehBEw3xtFbdJaodfGXgJDJmxSiEVuqZqTnxugXkblck3-BVS3dJcjyY6AJuuWI_Y24pvlFoeF_iikh6qdIgvVynnCKZi9fuuLgwk4BJDWBIIETct_EaVe9x3APujDnqeJdk3-P2spB1_IZrO1Q10TU1_iHlKoHDyaTvI_E0IPW60Jpo1egzJh90FUR6d46fLETv-Xi054EeeNafcN1CiIdIr_vc1xJrXYYtGey31ImJEaqqu9h2oRYiw8nds_RIm6gNyvuD4DElx7iq4-aPkA=s256-no?authuser=0',
+    },
+    price: 0.99,
+  },
+  {
+    id: 'recgdtfglGNGsO4Q5',
+    name: 'custard biscuits',
+    image: {
+      url:
+        'https://lh3.googleusercontent.com/eEoGAhTXg20CxnvH3a39yidqFg8zYRLEWp9WtKUoB00hZ2VZ0kjsxzxPKDyOOsluffoc9yGoCVHAXrF6hcGCVlZsoKQ8RkhPTRoZj2Z1mMWPayMTjO-r5XrHtyf8nUyNKNGZayDv1jWQQ_KFueU_RaB7EzNaxz6XdwbN3IDnpu1JKTEyaxMh4Z_VUqN6KAUnwIhArQIkqbe0kZcQaWXSV98Bp_vbRzOHsi0eZw1MjHlx4n0kCinEuy0RqdczZ3nOC4fIqgbbPc_i8Nb4_a4xafu_VecxOWk2L24qJUqbg3MQBt0ytcXWtA_e8b9bFiS-PTEgykzg2pRa5U8b9NvGPrJ2VMFVjvctALZrY7u89FEU9LJ-r0HKLzyXK16Gvlq9C_omLKCMNrLomq_V19gYLKxf6vgPY3xq6SDbvXos6ikG7iinixKKhnKA-bMsjbgf6k6xDtkQqJRtLEA51Wapmw4I3UuJNK_n6_3Y48FaU2kH_qN1xHbyJsjP87o7-4-kA0BfPKPtydxmdv1724ufUbwiKEOWHk0ltSl3UITZ-kGelcXAnB7K9sj_NaIw9MEBemrYB8RyAOc1PnbfRjjMlJdcqjB56Hy-FzF0oOiA9QNOPaskDgFon0_XIgfLesb7xFJDTrWahVxCgTSS_YOq4WxANo_KFOmlefWFvGTHqZ73Oj3R7ekQHjeiM8RvqC1RQq7LIfIrwWskV-1Ph43tDg=s1280-no?authuser=0',
+    },
+    price: 0.99,
   },
 ]
 
--- works/2-tkstore-api/app.js
+++ works/2-tkstore-api/app.js
@@ -23,3 +23,4 @@ const fetchData = async () => {
 }
 
 fetchData()
+
